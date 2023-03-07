4a85aed2e403c223bb489074a953e859028eb8cc
TheB1t0s <coimbrawork25@gmail.com>
2023-03-06 22:31:10 -0300

    Bot Cotação

--- BotCotacao.py
+++ BotCotacao.py
@@ -1,7 +1,16 @@
 import discord
+import requests
 
+link = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
 intents = discord.Intents.all()
 Client = discord.Client(command_prefix='!', intents=intents)
+
+moeda = requests.get(link)
+moeda = moeda.json() 
+moeda_dolar = moeda['USDBRL']['bid']
+moeda_euro = moeda['EURBRL']['bid']
+moeda_BTC = moeda['BTCBRL']['bid']
+
 
 @Client.event
 async def on_ready():
@@ -15,4 +24,13 @@ async def on_message(message):
   if message.content.startswith("Oi"):
     await message.channel.send('Oi tudo bem? Eu sou o EconBot vou auxiliar você em busca de informações')
 
-Client.run('MTA4MjQzMTA3MTg4MDIzMzAyMg.GGr25V.3z6mc0offaIWWHGnk_HqG8RyHqE9vbAxeQtkmg')
+  if message.content.startswith("Dolar"):
+     await message.channel.send(moeda_dolar)
+  
+  if message.content.startswith("Euro"):
+     await message.channel.send(moeda_euro)
+  
+  if message.content.startswith("bitcoin"):
+     await message.channel.send(moeda_BTC)
+
+Client.run('MTA4MjQzMTA3MTg4MDIzMzAyMg.GkngVj.xnv4QFqAm27GiW_cfUfmsfmTVzRaJqBVkg40lM')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Criação de um bot pro discord que vai ler a cotação do dia e informar aos usuários se perguntado.
+Criação de um bot pro discord que vai ler a cotação do dia baseado no real e informar aos usuários se perguntado.
+
+Algumas instruções
+1° Trocar o token do discord para usar no seu bot de discord.
+2° Mexer nas permissões do bot antes de adicionar ele ao seu servidor.
